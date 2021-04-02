11a947d9afa198f619c922819b3ad2e3884a3d1a
Iliyan Dimitrov <iliyanid2000@gmail.com>
2021-04-01 18:59:40 -0600

    Added Message Grouping

--- front-end/src/container/dashboard/dashboard.tsx
+++ front-end/src/container/dashboard/dashboard.tsx
@@ -32,9 +32,29 @@ const parseMessageData = (index:number,messageData,userName:string) =>{
         return false;
     }
 
+    const combineMessages = (index:number,currentMessages) => {
+        let user = currentMessages[index].userName;
+        let date = currentMessages[index].date;
+        let combine:JSX.Element[] = [];
+
+
+        for(let i = index;i < currentMessages.length; i++){
+            if((currentMessages[i].userName !== user) || (currentMessages[i].date !== date)){
+                index = i -1;
+                break;
+            }
+            else if(i === currentMessages.length - 1)
+                index = i;
+            combine.push(<div className='messageText'>{currentMessages[i].message}</div>);
+        }
+        return {i:index,combine:combine};
+    }
+
     let JSXmessagesArray:JSX.Element[] = [];
     let currentMessages = messageData[index];
     for(let i = 1; i < currentMessages.length;i++){
+        let messageResult = combineMessages(i,currentMessages);
+        
         let individualMessage = (
             <li>
                 <img className='profilePicture' src={(currentMessages[i].userName === userName)?HostProfile : ClientProfile} alt ='Host Profile'/>
@@ -42,14 +62,13 @@ const parseMessageData = (index:number,messageData,userName:string) =>{
                     <div className='hostName'>
                         {currentMessages[i].userName}
                     </div>
-                    <div className='time'>{(compareDates(currentMessages[i].date)) ? ("Today at" + currentMessages[i].time) : currentMessages[i].date}</div>
-                    <div className='messageText'>
-                        {currentMessages[i].message}
-                    </div>
+                    <div className='time'>{(compareDates(currentMessages[i].date)) ? ("Today at " + currentMessages[i].time) : currentMessages[i].date}</div>
+                    {messageResult.combine.map((jsx)=>{return jsx;})}
                 </div>
             </li>
         );
         JSXmessagesArray.push(individualMessage)
+        i = messageResult.i;
     }
 
     const parseParticipants = (users:string[]):string => {
@@ -64,7 +83,7 @@ const parseMessageData = (index:number,messageData,userName:string) =>{
     let result = (
         <Fragment>
             <div className='messageHeader'>
-                <div className = 'friendName'>{parseParticipants(currentMessages[0])}</div>
+                <div className = 'friendName'>{currentMessages[0].map((s)=>{return ('@' + s + ' ');})}</div>
                 <ul className='callOptions'>
                     <li><Phone className='svg microphone'/></li>
                     <li><Video className='svg'/></li>
@@ -88,13 +107,13 @@ const Dashboard: React.FC<props> = ({logOut,creds}) => {
     let messageData = 
 [
     [
-        ["User1"],
-        {userName:"User1", message:"This is what the client is saying", date:"3/20/2021", time:"10:26AM"},
-        {userName:"User1", message:"This is what the client is saying", date:"4/1/2021", time:"10:26AM"},
+        ["User"],
+        {userName:"User", message:"This is what the client is saying", date:"3/20/2021", time:"10:26AM"},
+        {userName:"User", message:"This is what the client is saying now", date:"4/1/2021", time:"10:26AM"},
         {userName:creds.userName, message:"This is what the user is responding with because because because because because because because because because because", date:"4/1/2021", time:"10:40AM"},
         {userName:creds.userName, message:"This is what the Host is saying", date:"4/1/2021", time:"10:40AM"},
-        {userName:"User1", message:"I am Responding to this", date:"4/1/2021", time:"10:41AM"},
-        {userName:"User1", message:"I disagree", date:"4/1/2021", time:"10:41AM"},
+        {userName:"User", message:"I am Responding to this", date:"4/1/2021", time:"10:41AM"},
+        {userName:"User", message:"I disagree", date:"4/1/2021", time:"10:41AM"}
         
     ]
 ]
@@ -132,41 +151,6 @@ const Dashboard: React.FC<props> = ({logOut,creds}) => {
     );
 }
 
-const dragElement = (element) =>{
-    const dragMouseDown = (e:any) =>{
-        e = e || window.event;
-        e.preventDefault();
 
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDrageElement;
-        document.onmousemove = elementDrag;
-    }
-
-    const elementDrag = (e) =>{
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        element.style.top = (element.offsetTop - pos2) + "px";
-        element.style.left = (element.offsetLeft - pos1) + "px";    
-    }
-
-    const closeDrageElement = () =>{
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-
-    let pos1 = 0,pos2 = 0, pos3 = 0, pos4 = 0;
-    let doc = document.getElementById("messagesHeader");
-    //if(doc != null)
-       // doc.onmousedown = dragMouseDown;
-    //else
-        element.onmousedown = dragMouseDown;
-}
 
 export default Dashboard;
